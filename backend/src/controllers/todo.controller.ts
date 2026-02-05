d85e810da8561c29b3c642b7084283df71fe9a22
Akash Chitale <akashchitale0@gmail.com>
2026-02-05 04:05:15 +0000

    feat: implement getTodo controller with JWT auth and Postman verification

--- backend/src/controllers/todo.controller.ts
+++ backend/src/controllers/todo.controller.ts
@@ -1,8 +1,16 @@
 import { Request, Response } from "express";
-// import { AuthRequest } from "../middlewares/auth.middleware.js";
+import { AuthRequest } from "../middlewares/auth.middleware.js";
+import Todo from "../models/todo.model.js";
 
-const getTodos = (req: Request, res: Response) => {
-  res.send('List of todos');
+const getTodos = async (req: AuthRequest, res: Response) => {
+  // res.send('List of todos');
+  try{
+    const userId = req.userId // Assuming req.user is set by auth middleware
+    const todos = await Todo.find({ userId });
+    res.status(200).json(todos);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to fetch todos", error });
+  }
 };
 
 export { getTodos };
--- backend/src/middlewares/auth.middleware.ts
+++ backend/src/middlewares/auth.middleware.ts
@@ -7,18 +7,20 @@ interface AuthRequest extends Request {
 
 const authMiddleware = (req: AuthRequest, res: Response, next: NextFunction) => {
     const authHeader = req.headers['authorization'];
+    // res.send('authHeader ' + authHeader);
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
-        return res.status(401).json({ message: "Unauthorized" });
+        return res.status(401).json({ message: "Unauthorized 1" });
     }
     const token = authHeader.split(' ')[1];
 
     if (!token) {
-        return res.status(401).json({ message: "Unauthorized" });
+        return res.status(401).json({ message: "Unauthorized 2" });
     }
 
     try {
         const secret = process.env.JWT_SECRET || "your_jwt_secret_key";
         const decoded = jwt.verify(token, secret) as { userId: string };
+        // res.send(decoded.userId);
         req.userId = decoded.userId;
         next();
     } catch (error) {
