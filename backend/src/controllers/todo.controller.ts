656d23e7b23fdaae7f7c7d5cfcfa09773b368599
Akash Chitale <akashchitale0@gmail.com>
2026-02-07 05:22:34 +0000

    feat: add update todo feature with validation

--- backend/src/controllers/todo.controller.ts
+++ backend/src/controllers/todo.controller.ts
@@ -65,4 +65,48 @@ const deleteTodo = async (req: AuthRequest, res: Response) => {
   }
 };
 
-export { getTodos, createTodo, deleteTodo };
+// update (/:id)
+const updateTodo = async (req: AuthRequest, res: Response) =>{
+  try {
+    const userId = req.userId;
+    const todoId = req.params.id as string;
+    if(!todoId || !mongoose.Types.ObjectId.isValid(todoId)) {
+      return res.status(400).json({ message: "Todo ID is required" });
+    }
+    const { title, description, dueDate } = req.body;
+    res.status(200).json({ title, description, dueDate });
+    if (dueDate){
+      const parsedDate = new Date(dueDate);
+      if (isNaN(parsedDate.getTime())) {
+        return res.status(400).json({ message: "Invalid due date format" });
+      }
+      if (parsedDate <= new Date()) {
+        return res.status(400).json({ message: "Due date must be in the future" });
+      }
+    }
+
+    if(!title){
+      return res.status(400).json({ message: "Title is required" });
+    }
+    
+    const todo = await Todo.findOne({ _id: todoId, userId }); 
+
+    if (!todo) {
+      return res.status(404).json({ message: "Todo not found" });
+    }
+
+    todo.title = title; // This . operator is used to update the title of the todo. It updated directly on the todo object which is an instance of the Todo model. After updating the properties, we call save() to persist the changes to the database.
+    if(todo.description !== undefined){ 
+      todo.description = description; 
+    }
+    if(dueDate !== undefined){
+      todo.dueDate = dueDate;
+    }
+    await todo.save();  // Save the updated todo
+    res.status(200).json(todo);
+} catch (error) {
+    res.status(500).json({ message: "Failed to update todo", error });
+  }
+};
+
+export { getTodos, createTodo, deleteTodo, updateTodo };
--- backend/src/routes/todo.route.ts
+++ backend/src/routes/todo.route.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
-import { getTodos, createTodo, deleteTodo } from "../controllers/todo.controller.js";
+import { getTodos, createTodo, deleteTodo, updateTodo } from "../controllers/todo.controller.js";
 import Todo from "../models/todo.model.js";
 
 const TodoRouter = Router();
@@ -10,5 +10,7 @@ TodoRouter.get("/", authMiddleware, getTodos);
 TodoRouter.post("/create", authMiddleware, createTodo);
 
 TodoRouter.delete("/:id", authMiddleware, deleteTodo);
+
+TodoRouter.put("/:id", authMiddleware, updateTodo);
 
 export default TodoRouter;
