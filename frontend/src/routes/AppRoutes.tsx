d7926eee4f1dc0384db0cea91e44725d423b0ff5
Akash Chitale <akashchitale0@gmail.com>
2026-02-22 07:52:03 +0000

    feat(auth): add protected routes for authenticated pages

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import TodosPage from '../pages/TodosPage';
+import LoginPage from '../pages/LoginPage';
+import { ProtectedRoute } from './ProtectedRoute';
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/todos" element={<TodosPage />} />
+        <Route path="/todos" element={<ProtectedRoute><TodosPage /></ProtectedRoute>} />
         <Route path="/" element={<h1>Hello</h1>} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
