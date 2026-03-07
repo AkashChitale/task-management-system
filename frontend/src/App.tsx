942db9681d56742a68b7b4b8fca8ff5c0a2b9088
Akash Chitale <akashchitale0@gmail.com>
2026-03-07 10:58:06 +0000

    refactor: move providers to App.tsx for better hierarchy

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,17 @@ import AppRoutes from "./routes/AppRoutes";
 import Navbar from "./components/Navbar";
 
 import "./App.css";
+import { AuthProvider } from "./auth/AuthContext";
 
 // axiosInstance.get("/").then(res => console.log("Hello from the backend!", res.data)).catch(console.error );
 
 function App() {
 
   return (
-    <>
+    <AuthProvider>
       <Navbar />
       <AppRoutes />
-    </>
+    </AuthProvider>
   )
 }
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { AuthProvider } from './auth/AuthContext';
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <AuthProvider>
+  <StrictMode>=
       <App />
-    </AuthProvider>
   </StrictMode>
 )
